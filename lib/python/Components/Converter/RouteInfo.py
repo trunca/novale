7c4461b7188002ef9c9c46aed05d1e37f47b15ed
Truncalinos <truncalinos@gmail.com>
2015-10-08 22:18:20 +0200

    actualizar

--- lib/python/Components/Converter/RouteInfo.py
+++ lib/python/Components/Converter/RouteInfo.py
@@ -1,27 +1,19 @@
-# RouteInfo
-# Copyright (c) 2boom 2012-13
-# v.0.8
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+# RouteInfo by 2boom 2012 v.0.5
 # <widget source="session.CurrentService" render="Label" position="189,397" zPosition="4" size="50,20" valign="center" halign="center" font="Regular;14" foregroundColor="foreground" transparent="1"  backgroundColor="background">
 #	<convert type="RouteInfo">Info</convert>
 # </widget>
 #<widget source="session.CurrentService" render="Pixmap" pixmap="750HD/icons/ico_lan_on.png" position="1103,35" zPosition="1" size="28,15" transparent="1" alphatest="blend">
-#    <convert type="RouteInfo">Lan  | Wifi | Modem</convert>
+#    <convert type="RouteInfo">Lan</convert>
 #    <convert type="ConditionalShowHide" />
 #  </widget>
-
+#<widget source="session.CurrentService" render="Pixmap" pixmap="750HD/icons/ico_wifi_on.png" position="1103,35" zPosition="2" size="28,15" transparent="1" alphatest="blend">
+#    <convert type="RouteInfo">Wifi</convert>
+#    <convert type="ConditionalShowHide" />
+#  </widget> 
+#<widget source="session.CurrentService" render="Pixmap" pixmap="750HD/icons/ico_3g_on.png" position="1103,35" zPosition="3" size="28,15" transparent="1" alphatest="blend">
+#    <convert type="RouteInfo">Modem</convert>
+#    <convert type="ConditionalShowHide" />
+#  </widget>
 
 from Components.Converter.Converter import Converter
 from Components.Element import cached
@@ -49,7 +41,7 @@ class RouteInfo(Converter, object):
 		for line in open("/proc/net/route"):
 			if self.type == self.Lan and line.split()[0] == "eth0" and line.split()[3] == "0003":
 				info = True
-			elif self.type == self.Wifi and (line.split()[0] == "wlan0" or line.split()[0] == "ra0") and line.split()[3] == "0003":
+			elif self.type == self.Wifi and line.split()[0] == "wlan0" and line.split()[3] == "0003":
 				info = True
 			elif self.type == self.Modem and line.split()[0] == "ppp0" and line.split()[3] == "0003":
 				info = True
@@ -63,7 +55,7 @@ class RouteInfo(Converter, object):
 		for line in open("/proc/net/route"):
 			if self.type == self.Info and line.split()[0] == "eth0" and line.split()[3] == "0003":
 				info = "lan"
-			elif self.type == self.Info and  (line.split()[0] == "wlan0" or line.split()[0] == "ra0") and line.split()[3] == "0003":
+			elif self.type == self.Info and line.split()[0] == "wlan0" and line.split()[3] == "0003":
 				info = "wifi"
 			elif self.type == self.Info and line.split()[0] == "ppp0" and line.split()[3] == "0003":
 				info = "3g"
