61e78d01af9a83775200aec603087274cbfea9cd
Truncalinos <truncalinos@gmail.com>
2015-10-13 17:49:23 +0200

    23

--- lib/dvb/lowlevel/mhw.h
+++ lib/dvb/lowlevel/mhw.h
@@ -55,10 +55,10 @@ typedef struct {
    u_char section_syntax_indicator               :1;
 #endif
   union {
-	u_char section_length_lo                 :8;
-	u_char mhw2_theme			              :8;
+	u_char section_length_lo                     :8;
+	u_char mhw2_theme			                 :8;
    };
-   u_char channel_id
+   u_char channel_id                             :8;
    union {
      u_char theme_id                             :8;
      u_char mhw2_hours                           :8;
