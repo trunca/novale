d42965483755def82680c0056906571f40590b89
Truncalinos <truncalinos@gmail.com>
2015-10-16 03:04:16 +0200

    crear

---
+++ box/box_spycat.py
@@ -0,0 +1,58 @@
+#Modulo falso para compatibilidad con Oe-Alliance
+
+def getMachineBuild():
+    return "spycat"
+
+def getMachineProcModel():
+    return "spycat"
+
+def getMachineBrand():
+    return "spycat"
+
+def getMachineName():
+    return "spycat"
+
+def getMachineMtdKernel():
+    return "mtd2"
+
+def getMachineKernelFile():
+    return "kernel.bin"
+
+def getMachineMtdRoot():
+    return "mtd0"
+
+def getMachineRootFile():
+    return "rootfs.bin"
+
+def getMachineMKUBIFS():
+    return "-m 2048 -e 126976 -c 4096"
+
+def getMachineUBINIZE():
+    return "-m 2048 -p 128KiB"
+
+def getBoxType():
+    return "spycat"
+
+def getBrandOEM():
+    return "spicat"
+
+def getOEVersion():
+    return "OE-SFteam 2.0"
+
+def getDriverDate():
+    return "20150610"
+
+def getImageVersion():
+    return "4.1.3"
+
+def getImageBuild():
+    return "1"
+
+def getImageDistro():
+    return "sfteam"
+
+def getImageFolder():
+    return spycat"
+
+def getImageFileSystem():
+    return "ubi"
